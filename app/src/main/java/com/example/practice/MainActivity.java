5a7062cd053f7fd116bb90ba1eec7cb29f0a614e
Khush Panchal <khush.panchal123@gmail.com>
2021-07-27 22:04:43 +0530

    Revert "Third Commit"
    This reverts commit 9b2dde18

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -12,6 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("This is second commit message");
-        This is intentionally putted error
     }
 }
