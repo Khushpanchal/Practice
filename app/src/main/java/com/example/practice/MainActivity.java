9b2dde1850bdcfb00573f40d9f0062901e5fa4c5
Khush Panchal <khush.panchal123@gmail.com>
2021-07-27 21:17:01 +0530

    Third Commit

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("This is second commit message");
+        This is intentionally putted error
     }
 }
